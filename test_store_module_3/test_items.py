65ad1e296efb655934a5348d162a06696140bd60
archont58 <emzot96@gmail.com>
2021-11-04 19:05:41 +0300

    Added Test of Web store

--- test_store_module_3/test_items.py
+++ test_store_module_3/test_items.py
@@ -4,11 +4,12 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_store(browser):
+def test_visible_button(browser):
+    submit_button = None
     browser.get(link)
     time.sleep(5)     # убедиться во время задержки, что язык страницы соответствует переданной локали
     try:
         submit_button = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
-    except NoSuchElementException:
-        submit_button = None
-    assert submit_button is not None, "The button was not found!"
+    finally:
+        assert submit_button is not None, "The button was not found!"
+
