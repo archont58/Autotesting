83aa0b6a7c5d8b37b800b4d32d57c1f0c7dddd5b
archont58 <emzot96@gmail.com>
2021-11-04 12:36:19 +0300

    update conftest.py

--- module_3/conftest.py
+++ module_3/conftest.py
@@ -1,11 +1,22 @@
 import pytest
 from selenium import webdriver
 
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default = "chrome", help="Choose browser: chrome or firefox")
+
 @pytest.fixture(scope="function")
-def browser():
-    print("Start browser")
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(10)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nStart Chrome...")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nStart Firefox...")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("Quit browser")
+    print(f"\n{browser_name} quit...")
     browser.quit()
