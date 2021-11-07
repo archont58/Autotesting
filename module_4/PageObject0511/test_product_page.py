7302b6e34352b6e8ebc9ea14af16499d26376fee
archont58 <emzot96@gmail.com>
2021-11-07 18:57:23 +0300

    added test for go to login page

--- module_4/PageObject0511/test_product_page.py
+++ module_4/PageObject0511/test_product_page.py
@@ -12,7 +12,7 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.basket_cost_equals_product_cost()
 
 
-#@pytest.mark.skip
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
      link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
      product_page = ProductPage(browser, link)
@@ -26,10 +26,23 @@ def test_guest_cant_see_success_message(browser):
     product_page.open()
     product_page.should_not_be_success_message()
 
-
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
     product_page.success_message_should_disappear()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.new
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
