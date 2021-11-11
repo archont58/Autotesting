0d0dab71e9490eead5a07fb35ef75da17e467042
archont58 <emzot96@gmail.com>
2021-11-11 20:01:23 +0300

    correction of a typo

--- Final_project/test_product_page.py
+++ Final_project/test_product_page.py
@@ -75,10 +75,19 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.need_review
+
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_should_be_empty()
+    basket_page.should_be_empty_basket_message()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, link_product)
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
